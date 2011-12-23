e3b062d59c75367d9b452136703222cf47386468
Nandor T. Szots <nandor@umbra-llc.com>
2011-12-23 09:07:38 -0800

    Initial Commit

---
+++ ISYController2/ISYControllerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ISYControllerAppDelegate.h
+//  ISYController2
+//
+//  Created by Nandor Szots on 12/23/11.
+//  Copyright (c) 2011 Umbra LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISYControllerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ISYController2/ISYControllerFirstViewController.m
@@ -0,0 +1,60 @@
+//
+//  ISYControllerFirstViewController.m
+//  ISYController2
+//
+//  Created by Nandor Szots on 12/23/11.
+//  Copyright (c) 2011 Umbra LLC. All rights reserved.
+//
+
+#import "ISYControllerFirstViewController.h"
+
+@implementation ISYControllerFirstViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
